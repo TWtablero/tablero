0b26fc00c0d583908fbc28f9f6010cd449d9694b
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2014-11-14 16:50:53 -0200

    client id change

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.clientId = process.env.CLIENT_ID || "12345abcde";
-config.clientSecret = process.env.CLIENT_SECRET || "12345abcde";
+config.clientId = process.env.CLIENT_ID || "2a046800354c5d6c4f19";
+config.clientSecret = process.env.CLIENT_SECRET || "d679146387e8be036d2f471efe309eaeff260fde";
 
 module.exports = config;
