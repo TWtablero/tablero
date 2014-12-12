46ec7ad94851ffb0a0b65e6a8d94fe5342197c19
Lucas Arruda <larruda@thoughtworks.com>
2014-12-12 15:11:05 -0200

    #103 - Adding GULP to build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,52 @@
 'use strict';
 
 var gulp = require('gulp');
-var refresh = require('gulp-livereload');
-var livereload = require('tiny-lr');
-var server = livereload();
+var rjs = require('gulp-requirejs');
+var uglify = require('gulp-uglify');
 
-gulp.task('livereload-server', function () {
-  server.listen(35729, function (err) {
-    if (err) { return console.log(err); }
-  });
-});
+gulp.task('rjs', function() {
+    rjs({
+      paths: {
+        underscore: "../bower_components/underscore/underscore",
+        jquery: "../bower_components/jquery/dist/jquery",
+        jqueryUI: "../bower_components/jquery-ui/jquery-ui",
+        hogan: "../bower_components/hogan",
+        flight: "../bower_components/flight",
+        blockUI: "../bower_components/blockui/jquery.blockUI",
+        bootstrap: "../bower_components/components-bootstrap/js/bootstrap",
+        config: 'config',
+        component: 'component',
+        page: 'page'
+      },
 
-gulp.task('css', function () {
-  gulp.src('app/**/*.css').pipe(refresh(server));
-});
+      shim: {
+        backbone: {
+          deps: ["jquery", "underscore"],
+          exports: "Backbone"
+        },
+        bootstrap: {
+          deps: ["jquery"]
+        },
+        jqueryUI: {
+          deps: ["jquery"],
+          exports: "jQuery"
+        },
+        underscore: {
+          exports: "_"
+        }
+      },
 
-gulp.task('js', function () {
-  gulp.src('app/**/*.js').pipe(refresh(server));
+      mainConfigFile : "app/js/main.js",
+      baseUrl : "app/js",
+      name: "main",
+      out: "app/dist/main.js",
+      removeCombined: true
+    })
+		.pipe(uglify())
+		.pipe(gulp.dest('.'));
 });
 
 gulp.task('default', function () {
-  gulp.run('livereload-server');
-
-  gulp.watch('app/**/*.css', function (event) {
-    gulp.run('css');
-  });
-
-  gulp.watch('app/**/*.js', function (event) {
-    gulp.run('js');
-  });
+  gulp.run('rjs');
+  
 });
