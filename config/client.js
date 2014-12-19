5c0f1c2d45aac9f74e4087bcc71e183c317a4cee
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-12-19 11:06:17 -0200

    Adding generic configuration variables

--- config/client.js
+++ config/client.js
@@ -10,4 +10,8 @@ addRepo('platform', 'PX_PLATFORM');
 addRepo('project-issues', 'PX_PROJECT_ISSUES');
 addRepo('pages', 'PX_PAGES');
 
+for(i = 0; i < 5; i++) {
+  addRepo(env['REPO_' + i + '_NAME'] || i + 'th', 'REPO_' + i + '_URL');
+}
+
 module.exports = {repos: repos};
