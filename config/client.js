1976012243638d2949aa10d5726836d94647fcee
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-12-19 15:19:47 -0200

    Sync config order with filter order

--- config/client.js
+++ config/client.js
@@ -4,10 +4,10 @@ var env = process.env;
 function addRepo(name, key) {
   env[key] && (repos[name] = env[key]);
 }
-addRepo('user-agent', 'PX_USER_AGENT');
-addRepo('dispatcher', 'PX_DISPATCHER');
-addRepo('platform', 'PX_PLATFORM');
 addRepo('project-issues', 'PX_PROJECT_ISSUES');
+addRepo('platform', 'PX_PLATFORM');
+addRepo('dispatcher', 'PX_DISPATCHER');
+addRepo('user-agent', 'PX_USER_AGENT');
 addRepo('pages', 'PX_PAGES');
 
 for(i = 0; i < 5; i++) {
