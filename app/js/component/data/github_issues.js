639089f14944d88f1fe931fee416bfe93d3317f7
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-01-13 17:29:41 -0200

    Fixing single repository issue fetching

--- app/js/component/data/github_issues.js
+++ app/js/component/data/github_issues.js
@@ -107,7 +107,7 @@ define([
         var names = _(issuesPromises).map(function(v,k) {return k;});
         $.when.apply(this, queries).done(
           function () {
-            var issuesResults = arguments;
+            var issuesResults = names.length > 1 ? arguments : [arguments];
             var projects = _(names).map(function(name, idx) {
               return {
                 'projectName': name,
